a6348f618e99d2571a1f9ec254dadaea98af1dea
Ananya jantli <jantliananya@gmail.com>
2025-11-20 14:24:09 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,13 @@
+import sys
+if len(sys.argv) != 3:
+  print ("usage: python student.py <name> <roll>")
+  sys.exit(1)
+
+script_name = sys.argv[0]
+name = sys.argv[1]
+rollno = sys.argv[1]
+
+print("sript name: ", script_name)
+print("student name: ", name)
+print("roll number: ", rollno)
+
